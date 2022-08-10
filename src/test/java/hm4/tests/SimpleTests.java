80b14494749ef69bdab76973437c09eaa6b52fdb
elevoshkin <elevoshkin@rvision.ru>
2022-08-10 20:33:10 +0300

    222

--- src/test/java/hm4/tests/SimpleTests.java
+++ src/test/java/hm4/tests/SimpleTests.java
@@ -4,6 +4,8 @@ import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
@@ -21,7 +23,9 @@ public class SimpleTests {
     void wikiTest() {
         open("selenide/selenide");
         $("#wiki-tab").click();
+        $("#wiki-pages-box").$(byText("Show 2 more pages…")).click();
         $("#wiki-body").find(byText("Soft assertions")).click();
-        $("#wiki-body").shouldHave(Condition.text("Using JUnit5 extend test class:"));
+        $("#wiki-body").shouldHave(text("Using JUnit5 extend test class:"));
+        $("#wiki-body").shouldHave(text("Using JUnit5 extend test class"));
     }
 }
