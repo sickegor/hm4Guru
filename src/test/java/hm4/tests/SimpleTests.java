e9dfc42e217474d038acd39d2fbc55807f6009f5
elevoshkin <elevoshkin@rvision.ru>
2022-08-10 18:24:14 +0300

    first commit

--- src/test/java/hm4/tests/SimpleTests.java
+++ src/test/java/hm4/tests/SimpleTests.java
@@ -2,13 +2,8 @@ package hm4.tests;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.WebDriverRunner;
-import org.junit.jupiter.api.*;
-
-import java.io.File;
-
-import static com.codeborne.selenide.Condition.text;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
@@ -23,19 +18,11 @@ public class SimpleTests {
         Configuration.browserPosition = "0x0";
     }
 
-    /**
-     * Проверка наличия примера для JUnit5 в Selenide Wiki page
-     */
     @Test
     void wikiJUnit5Test() {
-        //Открываем страницу Selenide
         open("selenide/selenide");
-        //Открываем Wiki Page
         $("#wiki-tab").click();
-        //Находим поле "Soft assertions" и проходим по ссылке
         $("#wiki-body").find(byText("Soft assertions")).click();
-        //Убеждаемся что на странице есть пример для JUnit5
         $("#wiki-body").shouldHave(Condition.text("Using JUnit5 extend test class:"));
     }
-
 }
